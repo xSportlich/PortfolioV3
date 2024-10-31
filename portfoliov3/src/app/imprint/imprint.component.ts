9377c47783459d94429dddb9126626f7783fb41c
Eduard Kammerzel <likeaedu@gmail.com>
2024-10-31 15:07:18 +0100

    bug fixed

--- portfoliov3/src/app/imprint/imprint.component.ts
+++ portfoliov3/src/app/imprint/imprint.component.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
   imports: [
-    FooterComponent
+    FooterComponent,
+    TranslateModule
   ],
   templateUrl: './imprint.component.html',
   styleUrls: ['./imprint.component.scss', '../shared/footer/footer.component.scss']
 
 })
 export class ImprintComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+  }
 
+  switchLanguegs(language: string) {
+
+    let english: any = document.getElementById('english');
+    let german: any = document.getElementById('german');
+
+
+    this.translate.use(language);
+
+    if (language == 'en') {
+      english.classList.add('point');
+    } else {
+      english.classList.remove('point');
+    } if (language == 'de') {
+      german.classList.add('point');
+    } else {
+      german.classList.remove('point');
+    }
+  }
 }
--- portfoliov3/src/app/policy/policy.component.ts
+++ portfoliov3/src/app/policy/policy.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-policy',
   standalone: true,
   imports: [
-    FooterComponent
+    FooterComponent,
+    TranslateModule
   ],
   templateUrl: './policy.component.html',
   styleUrl: './policy.component.scss'
 })
 export class PolicyComponent {
-
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+  }
 }
